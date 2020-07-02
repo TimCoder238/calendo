d2aae7622798608b91451de9526ffdc1e7d9ae0f
Tim Costa <costa.tim@live.be>
2020-07-02 23:46:45 +0200

    Updates

---
+++ precache-manifest.d09ec2a5357bd0d7c67cc67d437d36e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810c0a5a1f262e68c7c140e860569d43",
+    "url": "/calendo/index.html"
+  },
+  {
+    "revision": "ed875127ce88e6470a3f",
+    "url": "/calendo/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "85d4710a24ead3011ea1",
+    "url": "/calendo/static/css/main.b425549f.chunk.css"
+  },
+  {
+    "revision": "ed875127ce88e6470a3f",
+    "url": "/calendo/static/js/2.5689063e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/calendo/static/js/2.5689063e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d4710a24ead3011ea1",
+    "url": "/calendo/static/js/main.d97807f4.chunk.js"
+  },
+  {
+    "revision": "86a4dd0542e2369a186d",
+    "url": "/calendo/static/js/runtime-main.04cc6907.js"
+  },
+  {
+    "revision": "ebd45221a875428c94508126b606f761",
+    "url": "/calendo/static/media/demo.ebd45221.png"
+  }
+]);
